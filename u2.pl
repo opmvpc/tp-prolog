7578df0e8ee670a58dccc417f1c6aa789135d842
opmvpc <thibsix@outlook.be>
2020-12-16 17:17:04 +0100

    fin refactor

--- u2.pl
+++ u2.pl
@@ -78,12 +78,16 @@ descend_du_pont(0, 0, 1, 1, 0, 0, 1, 1).
 % Prédicats permettant de vérifier si la contrainte de temps est respectée
 %===========================================================================
 
+% Récupération du temps avant le concert
+%
+% temps_avant_concert(Minutes)
+%
 temps_avant_concert(Minutes) :- Minutes = 17.
 
 % Vérification de la contrainte de temps avant le début du concert
 %
 % a_temps_pour_concert(Temps_total)
-
+%
 a_temps_pour_concert(Temps_total) :-
     temps_avant_concert(Minutes),
     Temps_total =< Minutes.
@@ -95,7 +99,7 @@ a_temps_pour_concert(Temps_total) :-
 %                     AdamTraverse,
 %                     LarryTraverse,
 %                     MaxTempsTraversee)
-
+%
 max_temps_traversee(BonoTraverse, EdgeTraverse, AdamTraverse, LarryTraverse, MaxTempsTraversee) :-
     ajouter_a_liste_si_different_de_zero(BonoTraverse, 1, [], Liste1),
     ajouter_a_liste_si_different_de_zero(EdgeTraverse, 2, Liste1, Liste2),
@@ -105,6 +109,11 @@ max_temps_traversee(BonoTraverse, EdgeTraverse, AdamTraverse, LarryTraverse, Max
     NbrPersonnes =< 2,
     max_list(Liste4, MaxTempsTraversee).
 
+% ajouter_a_liste_si_different_de_zero(PersonneEstPresente,
+%                     TempsTraversee,
+%                     Liste,
+%                     ListeRetour)
+%
 ajouter_a_liste_si_different_de_zero(PersonneEstPresente, TempsTraversee, Liste, ListeRetour) :-
     PersonneEstPresente == 1 -> append(Liste, [TempsTraversee], ListeRetour) ; ListeRetour = Liste.
 
@@ -112,21 +121,23 @@ ajouter_a_liste_si_different_de_zero(PersonneEstPresente, TempsTraversee, Liste,
 % Prédicats décrivant les différentes transitions possibles
 %===========================================================================
 
-% Une personne monte sur le pont
-% 2 cas: Lampe de poche à gauche ou à droite
+% Une personne monte sur le pont depuis le côté gauche (cas aller)
+% Lampe de poche à gauche
 %
 % transition(EtatDepart,
 %            EtatArrivee,
-%            MonterPont)
-transition( etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, gauche, T),
-            etat(Bfg, Efg, Afg, Lfg, Bd, Ed, Ad, Ld, Bfp, Efp, Afp, Lfp, gauche, T),
-            monter_pont(I, J, K, L) ) :-
+%            Action)
+%
+transition(etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, gauche, Temps),
+           etat(Bfg, Efg, Afg, Lfg, Bd, Ed, Ad, Ld, Bfp, Efp, Afp, Lfp, gauche, Temps),
+           monter_pont(I, J, K, L)) :-
 
-    Nb1 is Bp + Ep,
-    Nb2 is Ap + Lp,
-    Nb is Nb1 + Nb2,
-    monte_sur_le_pont(Nb, I, J, K, L),
+    NbrPersonnesSurPont is Bp + Ep + Ap + Lp,
+    % on vérifie qu'un prédicat autorise cette action
+    monte_sur_le_pont(NbrPersonnesSurPont, I, J, K, L),
+    % une personne qui monte sur le pont doit se trouver du côté gauche
     I =< Bg, J =< Eg, K =< Ag, L =< Lg,
+    % On calcul la valeur des paramètres de l'état d'arrivée
     Bfg is Bg - I,
     Efg is Eg - J,
     Afg is Ag - K,
@@ -136,113 +147,183 @@ transition( etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, gauche, T),
     Afp is Ap + K,
     Lfp is Lp + L.
 
-transition( etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, droite, T),
-            etat(Bg, Eg, Ag, Lg, Bfd, Efd, Afd, Lfd, Bfp, Efp, Afp, Lfp, droite, T),
-            monter_pont(I, J, K, L) ) :-
-                Nb1 is Bp + Ep,
-                Nb2 is Ap + Lp,
-                Nb is Nb1 + Nb2,
-                monte_sur_le_pont(Nb, I, J, K, L),
-                I =< Bd, J =< Ed, K =< Ad, L =< Ld,
-                Bfd is Bd - I,
-                Efd is Ed - J,
-                Afd is Ad - K,
-                Lfd is Ld - L,
-                Bfp is Bp + I,
-                Efp is Ep + J,
-                Afp is Ap + K,
-                Lfp is Lp + L.
+% Une personne monte sur le pont depuis le côté droit (cas retour)
+% Lampe de poche à droite
+%
+% transition(EtatDepart,
+%            EtatArrivee,
+%            Action)
+%
+transition(etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, droite, Temps),
+           etat(Bg, Eg, Ag, Lg, Bfd, Efd, Afd, Lfd, Bfp, Efp, Afp, Lfp, droite, Temps),
+           monter_pont(I, J, K, L)) :-
 
-% descend_du_pont du pont
-transition( etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, gauche, T),
-            etat(Bfg, Efg, Afg, Lfg, Bd, Ed, Ad, Ld, Bfp, Efp, Afp, Lfp, gauche, T),
-            descendre_pont(I, J, K, L) ) :-
-                descend_du_pont(Bp, Ep, Ap, Lp, I, J, K, L),
-                Bfg is Bg + I,
-                Efg is Eg + J,
-                Afg is Ag + K,
-                Lfg is Lg + L,
-                Bfp is Bp - I,
-                Efp is Ep - J,
-                Afp is Ap - K,
-                Lfp is Lp - L.
+    NbrPersonnesSurPont is Bp + Ep + Ap + Lp,
+    % on vérifie qu'un prédicat autorise cette action
+    monte_sur_le_pont(NbrPersonnesSurPont, I, J, K, L),
+    % une personne qui monte sur le pont doit se trouver du côté droit
+    I =< Bd, J =< Ed, K =< Ad, L =< Ld,
+    % On calcul la valeur des paramètres de l'état d'arrivée
+    Bfd is Bd - I,
+    Efd is Ed - J,
+    Afd is Ad - K,
+    Lfd is Ld - L,
+    Bfp is Bp + I,
+    Efp is Ep + J,
+    Afp is Ap + K,
+    Lfp is Lp + L.
 
-transition( etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, droite, T),
-            etat(Bg, Eg, Ag, Lg, Bfd, Efd, Afd, Lfd, Bfp, Efp, Afp, Lfp, droite, T),
-            descendre_pont(I, J, K, L) ) :-
-                descend_du_pont(Bp, Ep, Ap, Lp, I, J, K, L),
-                Bfd is Bd + I,
-                Efd is Ed + J,
-                Afd is Ad + K,
-                Lfd is Ld + L,
-                Bfp is Bp - I,
-                Efp is Ep - J,
-                Afp is Ap - K,
-                Lfp is Lp - L.
+% Une personne descend du côté gauche du pont (cas retour)
+% Lampe de poche à gauche
+%
+% transition(EtatDepart,
+%            EtatArrivee,
+%            Action)
+%
+transition(etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, gauche, Temps),
+           etat(Bfg, Efg, Afg, Lfg, Bd, Ed, Ad, Ld, Bfp, Efp, Afp, Lfp, gauche, Temps),
+           descendre_pont(I, J, K, L)) :-
 
-% Traverser le pont
-transition( etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, gauche, T),
-            etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, droite, Tf),
-            traverser ) :-
-                Nb1 is Bp + Ep,
-                Nb2 is Ap + Lp,
-                Nb is Nb1 + Nb2,
-                1 =< Nb,
-                Nb =< 2,
-                max_temps_traversee(Bp, Ep, Ap, Lp, X),
-                Tf is T + X,
-                a_temps_pour_concert(Tf).
+    % on vérifie qu'un prédicat autorise cette action
+    descend_du_pont(Bp, Ep, Ap, Lp, I, J, K, L),
+    % On calcul la valeur des paramètres de l'état d'arrivée
+    Bfg is Bg + I,
+    Efg is Eg + J,
+    Afg is Ag + K,
+    Lfg is Lg + L,
+    Bfp is Bp - I,
+    Efp is Ep - J,
+    Afp is Ap - K,
+    Lfp is Lp - L.
 
-transition( etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, droite, T),
-            etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, gauche, Tf),
-            traverser ) :-
-                Nb1 is Bp + Ep,
-                Nb2 is Ap + Lp,
-                Nb is Nb1 + Nb2,
-                1 =< Nb,
-                Nb =< 2,
-                max_temps_traversee(Bp, Ep, Ap, Lp, X),
-                Tf is T + X,
-                a_temps_pour_concert(Tf).
+% Une personne descend du côté droit du pont (cas aller)
+% Lampe de poche à droite
+%
+% transition(EtatDepart,
+%            EtatArrivee,
+%            Action)
+%
+transition(etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, droite, Temps),
+           etat(Bg, Eg, Ag, Lg, Bfd, Efd, Afd, Lfd, Bfp, Efp, Afp, Lfp, droite, Temps),
+           descendre_pont(I, J, K, L)) :-
 
-% Affichage
+    % on vérifie qu'un prédicat autorise cette action
+    descend_du_pont(Bp, Ep, Ap, Lp, I, J, K, L),
+    % On calcul la valeur des paramètres de l'état d'arrivée
+    Bfd is Bd + I,
+    Efd is Ed + J,
+    Afd is Ad + K,
+    Lfd is Ld + L,
+    Bfp is Bp - I,
+    Efp is Ep - J,
+    Afp is Ap - K,
+    Lfp is Lp - L.
+
+% Une personne traverse le pont de gauche à droite (cas aller)
+%
+% transition(EtatDepart,
+%            EtatArrivee,
+%            Action)
+%
+transition(etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, gauche, Temps),
+           etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, droite, TotalTemps),
+           traverser) :-
+
+    % Calcul du temps de la traversée du pont
+    max_temps_traversee(Bp, Ep, Ap, Lp, MaxTempsTraversee),
+    % On additionne avec le temps déjà écoulé
+    TotalTemps is Temps + MaxTempsTraversee,
+    % On vérifie qu'on est toujours dans les temps pour le concert
+    a_temps_pour_concert(TotalTemps).
+
+% Une personne traverse le pont de droite à gauche (cas retour)
+%
+% transition(EtatDepart,
+%            EtatArrivee,
+%            Action)
+%
+transition(etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, droite, Temps),
+           etat(Bg, Eg, Ag, Lg, Bd, Ed, Ad, Ld, Bp, Ep, Ap, Lp, gauche, TotalTemps),
+           traverser) :-
+
+    % Calcul du temps de la traversée du pont
+    max_temps_traversee(Bp, Ep, Ap, Lp, MaxTempsTraversee),
+    % On additionne avec le temps déjà écoulé
+    TotalTemps is Temps + MaxTempsTraversee,
+    % On vérifie qu'on est toujours dans les temps pour le concert
+    a_temps_pour_concert(TotalTemps).
+
+%===========================================================================
+% Prédicats utilisés pour afficher les solutions
+%===========================================================================
+
+% Affiche le contenu d'une liste
+%
+% ecrire(Liste)
+%
 ecrire([]).
-ecrire([H|T]) :- write(H), nl, ecrire(T).
 
-ecrireTransitions([]).
-ecrireTransitions([H|T]) :- ecrire(H), nl, nl, ecrireTransitions(T).
+ecrire([H|T]) :-
+    write(H), nl, ecrire(T).
 
-% Recherche des solutions
+% Affiche le contenu d'une liste de Transitions
+%
+% ecrireTransitions(Transitions, ListeTransitions)
+%
+ecrireTransitions(TransitionsFinales, []).
+ecrireTransitions(TransitionsFinales, [H|T]) :-
+    % nth0(NumTransition, _, H),
+    % nl, ecrire("Solution #"), ecrire(NumTransition), nl, nl,
+    ecrire(H), nl, nl,
+    ecrireTransitions(TransitionsFinales, T).
+
+%===========================================================================
+% Prédicats décrivant la recherche de solutions valides
+%===========================================================================
+
 recherche(Etat, Etat, []).
-recherche(Init, Final, [monter_pont(I, J, K, L),
-               traverser, descendre_pont(P, Q, R, S)]) :-
-    transition(Init, Tmp1, monter_pont(I, J, K, L)),
-    transition(Tmp1, Tmp2, traverser),
-    transition(Tmp2, Final, descendre_pont(P, Q, R, S)).
-recherche(Init, Final, [monter_pont(I, J, K, L),
-               traverser, descendre_pont(P, Q, R, S)|Trav]) :-
-    transition(Init, Tmp1, monter_pont(I, J, K, L)),
-    transition(Tmp1, Tmp2, traverser),
-    transition(Tmp2, Tmp3, descendre_pont(P, Q, R, S)),
-    recherche(Tmp3, Final, Trav).
 
-% Point d'entrée du test
-trouver_solutions :- setof(Transitions, recherche( etat(1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, gauche, 0),
-    etat(0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, droite, X),
-    Transitions ), SetTransitions),
-    ecrireTransitions(SetTransitions),
-    write('La traversee s\'est faite en : '),
-    write(X),
-    write(' minutes.\n\n').
+recherche(Init, Final, [
+        monter_pont(I, J, K, L),
+        traverser,
+        descendre_pont(P, Q, R, S)
+        |T
+    ]) :-
+
+    transition(Init, Aux, monter_pont(I, J, K, L)),
+    transition(Aux, Aaux, traverser),
+    transition(Aaux, Aaaux, descendre_pont(P, Q, R, S)),
+    recherche(Aaaux, Final, T).
+
+%===========================================================================
+% Prédicat lancant la recherche de solutions
+%===========================================================================
+
+trouver_solutions :-
+    setof(
+        Transitions,
+        recherche(
+            etat(1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, gauche, 0),
+            etat(0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, droite, _),
+            Transitions),
+        SetTransitions),
+    ecrireTransitions(SetTransitions, SetTransitions).
+
+%===========================================================================
+% Prédicat permettant de vérifier si une solution est valide
+%===========================================================================
 
 solution_est_valide(Transitions) :-
+
     temps_avant_concert(Minutes),
-    EstValide = recherche(etat(1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, gauche, 0),
+    EstValide = recherche(
+        etat(1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, gauche, 0),
         etat(0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, droite, Minutes),
-    Transitions),
+        Transitions),
     EstValide -> (write("La solution est valide!"), true) ; (write("La solution n'est pas valide!"), false).
 
 exemple_solution_est_valide :-
+
     solution_est_valide([
         monter_pont(1,1,0,0),
         traverser,
